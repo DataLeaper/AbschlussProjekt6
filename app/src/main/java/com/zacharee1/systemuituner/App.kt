9003eb3b5a35790fe39bdc78499620d643598c16
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-09-27 21:51:43 +0000

    Edited README

--- app/src/main/java/com/zacharee1/systemuituner/App.kt
+++ app/src/main/java/com/zacharee1/systemuituner/App.kt
@@ -12,4 +12,22 @@ import android.util.Log
 import androidx.core.content.ContextCompat
 import com.bugsnag.android.Bugsnag
 import com.zacharee1.systemuituner.services.Manager
-import com.zacharee1.systemui
+import com.zacharee1.systemuituner.util.PersistenceHandlerRegistry
+import com.zacharee1.systemuituner.util.PrefManager
+import com.zacharee1.systemuituner.util.prefManager
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import kotlin.system.exitProcess
+
+class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
+    companion object {
+        private val connection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {}
+            override fun onServiceDisconnected(name: ComponentName?) {}
+        }
+
+        fun updateServiceState(context: Context) {
+            with (context) {
+                if (prefManager.persistentOptions.isEmpty()) {
+                    try {
+                        unbindService(connection)
+                    } catch (_: Il
