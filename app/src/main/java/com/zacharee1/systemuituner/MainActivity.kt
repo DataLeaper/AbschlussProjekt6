1a26570c234c495ec1bde8c2b4b6fc253b14366c
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-10-18 22:56:22 +0000

    Basic readme

--- app/src/main/java/com/zacharee1/systemuituner/MainActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/MainActivity.kt
@@ -9,4 +9,21 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.*
 import android.view.animation.AccelerateInterpolator
-import android.view.animation.An
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
+import androidx.core.os.bundleOf
+import androidx.core.view.updateLayoutParams
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import com.zacharee1.systemuituner.activities.intro.ComposeIntroActivity
+import com.zacharee1.systemuituner.databinding.ActivityMainBinding
+import com.zacharee1.systemuituner.fragments.BasePrefFragment
+import com.zacharee1.systemuituner.fragments.HomeFragment
+import com.zacharee1.systemuituner.fragments.SearchFragment
+import com.zacharee1.systemuituner.util.*
+
+class MainActivity : AppCompatActivity(), NavControl
