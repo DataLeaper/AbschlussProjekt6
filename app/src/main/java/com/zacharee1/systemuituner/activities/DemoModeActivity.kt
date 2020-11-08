ae85f311f45c4edc9c04c799456652a7bc6818d7
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-11-08 08:38:18 +0000

    Release 2.1.1

--- app/src/main/java/com/zacharee1/systemuituner/activities/DemoModeActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/DemoModeActivity.kt
@@ -9,4 +9,34 @@ import com.zacharee1.systemuituner.activities.intro.ComposeIntroActivity
 import com.zacharee1.systemuituner.databinding.ActivityDemoModeBinding
 import com.zacharee1.systemuituner.dialogs.RoundedBottomSheetDialog
 import com.zacharee1.systemuituner.util.addAnimation
-import com.zacharee1.systemuituner.util.hasDu
+import com.zacharee1.systemuituner.util.hasDump
+
+class DemoModeActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityDemoModeBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.addAnimation()
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (!hasDump) {
+            ComposeIntroActivity.start(this, arrayOf(ComposeIntroActivity.Companion.StartReason.EXTRA_PERMISSIONS))
+            finish()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+ 
