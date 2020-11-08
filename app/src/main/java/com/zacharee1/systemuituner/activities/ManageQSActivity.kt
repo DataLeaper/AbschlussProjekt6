9cdef92d47d9e0adc591409a3962120c88ef8abd
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-11-08 21:34:41 +0000

    update main.css

--- app/src/main/java/com/zacharee1/systemuituner/activities/ManageQSActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/ManageQSActivity.kt
@@ -2,4 +2,26 @@ package com.zacharee1.systemuituner.activities
 
 import android.os.Bundle
 import android.view.Menu
-import 
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.databinding.ActivityManageQsBinding
+import com.zacharee1.systemuituner.dialogs.RoundedBottomSheetDialog
+import com.zacharee1.systemuituner.util.addAnimation
+
+class ManageQSActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityManageQsBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.addAnimation()
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       
