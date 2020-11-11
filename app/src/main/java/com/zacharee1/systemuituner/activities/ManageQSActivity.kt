e8fce0d92d29d30daeedbf1f9a442181b62211a1
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-11-11 22:27:53 +0000

    error correction

--- app/src/main/java/com/zacharee1/systemuituner/activities/ManageQSActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/ManageQSActivity.kt
@@ -24,4 +24,28 @@ class ManageQSActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-       
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_help, menu)
+
+        val helpItem = menu.findItem(R.id.help)
+        helpItem.isVisible = true
+        helpItem.setOnMenuItemClickListener {
+            RoundedBottomSheetDialog(this).apply {
+                setTitle(R.string.help)
+                setMessage(R.string.option_advanced_manage_qs_tiles_desc)
+                setPositiveButton(android.R.string.ok, null)
+                show()
+            }
+            false
+        }
+
+        return true
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/activities/PersistentActivity.kt
@@ -0,0 +1,5 @@
+package com.zacharee1.systemuituner.activities
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuIt
