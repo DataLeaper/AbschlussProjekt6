b572e4f1376aba31c8b8afb28cc3c5355bee154a
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-11-16 06:36:59 +0000

    vc

--- app/src/main/java/com/zacharee1/systemuituner/activities/PersistentActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/PersistentActivity.kt
@@ -2,4 +2,21 @@ package com.zacharee1.systemuituner.activities
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuIt
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.databinding.ActivityPersistentBinding
+import com.zacharee1.systemuituner.dialogs.RoundedBottomSheetDialog
+import com.zacharee1.systemuituner.fragments.PersistentFragment
+import com.zacharee1.systemuituner.util.addAnimation
+
+class PersistentActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityPersistentBinding.inflate(layoutInflater) }
+    private val persistentFragment by lazy { supportFragmentManager.findFragmentById(R.id.persistent_fragment) as PersistentFragment }
+    private var searchView: SearchView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent
