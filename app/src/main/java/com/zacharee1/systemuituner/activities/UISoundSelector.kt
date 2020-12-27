64130fe226b7bf740965deb464db2236ee153f30
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2020-12-27 09:02:10 +0000

    Maven artifacts for SWI Prolog Enabler version 1.1.4-SNAPSHOT

--- app/src/main/java/com/zacharee1/systemuituner/activities/UISoundSelector.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/UISoundSelector.kt
@@ -8,4 +8,23 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.FileUtils
-impo
+import android.system.Os
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.zacharee1.systemuituner.IUISoundSelectionCallback
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.util.callSafely
+import java.io.File
+import java.io.IOException
+
+class UISoundSelector : AppCompatActivity() {
+    companion object {
+        private const val EXTRA_KEY = "key"
+        private const val EXTRA_CALLBACK = "callback"
+
+        fun start(context: Context, key: String, callback: IUISoundSelectionCallback) {
+            val activity = Intent(context, UISoundSelector::class.java)
+            activity.putExtra(EXTRA_KEY, key)
+            activity.
