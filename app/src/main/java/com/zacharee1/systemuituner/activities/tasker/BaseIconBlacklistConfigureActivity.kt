d7261824439ea0100001fa946f5904a72197a6f6
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-01-10 08:16:01 +0000

    chore: prepare next development for new release

--- app/src/main/java/com/zacharee1/systemuituner/activities/tasker/BaseIconBlacklistConfigureActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/tasker/BaseIconBlacklistConfigureActivity.kt
@@ -4,4 +4,23 @@ import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import com.joaomgcd.taskerpluginlibrary.c
+import com.joaomgcd.taskerpluginlibrary.config.TaskerPluginConfig
+import com.joaomgcd.taskerpluginlibrary.input.TaskerInput
+import com.zacharee1.systemuituner.data.tasker.TaskerIconBlacklistData
+import com.zacharee1.systemuituner.databinding.ActivityTaskerIconBlacklistInputBinding
+import com.zacharee1.systemuituner.util.tasker.helpers.IconBlacklistHelper
+
+open class BaseIconBlacklistConfigureActivity(private val isRemove: Boolean) : AppCompatActivity(),
+    TaskerPluginConfig<TaskerIconBlacklistData> {
+    override val context: Context
+        get() = this
+    override val inputForTasker: TaskerInput<TaskerIconBlacklistData>
+        get() = TaskerInput(
+            TaskerIconBlacklistData(
+                isRemove = isRemove,
+                key = key
+            )
+        )
+
+    private val helper by lazy { IconBlacklistHelper(this) }
+    protected val binding by lazy { Activ
