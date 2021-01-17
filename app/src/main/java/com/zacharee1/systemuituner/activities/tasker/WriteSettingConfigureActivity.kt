5bc48c4974f1224ade55ba65191f3490a5fa81dd
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-01-17 06:40:03 +0000

    Replace git.openstack.org URLs with opendev.org URLs

--- app/src/main/java/com/zacharee1/systemuituner/activities/tasker/WriteSettingConfigureActivity.kt
+++ app/src/main/java/com/zacharee1/systemuituner/activities/tasker/WriteSettingConfigureActivity.kt
@@ -1,3 +1,20 @@
 package com.zacharee1.systemuituner.activities.tasker
 
-import android.conte
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.joaomgcd.taskerpluginlibrary.config.TaskerPluginConfig
+import com.joaomgcd.taskerpluginlibrary.input.TaskerInput
+import com.zacharee1.systemuituner.data.tasker.TaskerWriteSettingData
+import com.zacharee1.systemuituner.databinding.TaskerWriteSettingBinding
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.tasker.helpers.WriteSettingHelper
+
+class WriteSettingConfigureActivity : AppCompatActivity(), TaskerPluginConfig<TaskerWriteSettingData> {
+    override val context: Context
+        get() = this
+    override val inputForTasker: TaskerInput<TaskerWriteSettingData>
+        get() = TaskerInput(TaskerWriteSettingData(type, key, value))
+
+    private var type: String = SettingsType.UNDEFINED.toString()
+    p
