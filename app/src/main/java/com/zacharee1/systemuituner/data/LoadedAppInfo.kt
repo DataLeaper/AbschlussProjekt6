5d3076e962a6821d2818db982b8175c25516d12a
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-01-24 14:50:53 +0000

    Fixed a memory leak.

---
+++ app/src/main/java/com/zacharee1/systemuituner/data/LoadedAppInfo.kt
@@ -0,0 +1,10 @@
+
+package com.zacharee1.systemuituner.data
+
+import android.graphics.drawable.Drawable
+
+data class LoadedAppInfo(
+    val label: String,
+    val packageName: String,
+    val icon: Drawable,
+    val colorPrimary: Int,
