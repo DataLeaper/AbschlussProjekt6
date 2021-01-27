95bcae2bbf113cfd3ea4b088be66bf2ad7351007
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-01-27 16:45:54 +0000

    Created LICENSE.txt

--- app/src/main/java/com/zacharee1/systemuituner/data/QSTileInfo.kt
+++ app/src/main/java/com/zacharee1/systemuituner/data/QSTileInfo.kt
@@ -74,3 +74,80 @@ class QSTileInfo(
                 component?.className?.split(".")?.run { this[size - 1] } ?: packageName
             } catch (e: Exception) {
                 packageName
+            }
+        }
+    }
+
+    fun getNameAndComponentForCustom(): ComponentName? {
+        val p = Pattern.compile("\\((.*?)\\)")
+        val m = p.matcher(key)
+
+        var name = ""
+
+        while (!m.hitEnd()) {
+            if (m.find()) name = m.group()
+        }
+
+        name = name.replace("(", "").replace(")", "")
+
+        return ComponentName.unflattenFromString(name)
+    }
+
+    private fun Context.getCustomDrawable(): Drawable? {
+        val component = getNameAndComponentForCustom()
+
+        return try {
+            packageManager.getServiceInfoCompat(component ?: throw NullPointerException("No component name found")).loadIcon(packageManager)
+        } catch (e: Exception) {
+            try {
+                packageManager.getApplicationInfoCompat(packageName).loadIcon(packageManager)
+            } catch (e: Exception) {
+                getDefaultDrawable()
+            }
+        }?.mutate()
+    }
+
+    private fun Context.chooseStandardDrawable(): Drawable? {
+        return ResourcesCompat.getDrawable(
+            resources,
+            when (key.lowercase()) {
+                "wifi" -> R.drawable.ic_baseline_signal_wifi_4_bar_24
+                "bluetooth", "bt" -> R.drawable.ic_baseline_bluetooth_24
+                "color_inversion" -> R.drawable.ic_baseline_invert_colors_24
+                "cell" -> R.drawable.ic_baseline_signal_cellular_4_bar_24
+                "do_not_disturb", "dnd" -> R.drawable.do_not_disturb
+                "airplane", "airplanemode" -> R.drawable.ic_baseline_airplanemode_active_24
+                "cast" -> R.drawable.ic_baseline_cast_24
+                "location" -> R.drawable.ic_baseline_location_on_24
+                "rotation", "rotationlock" -> R.drawable.ic_baseline_screen_rotation_24
+                "flashlight" -> R.drawable.ic_baseline_highlight_24
+                "hotspot" -> R.drawable.ic_baseline_wifi_tethering_24
+                "battery" -> R.drawable.battery_full
+                "sound", "soundmode" -> R.drawable.ic_baseline_volume_up_24
+                "sync" -> R.drawable.ic_baseline_sync_24
+                "nfc" -> R.drawable.ic_baseline_nfc_24
+                "data", "mobiledata" -> R.drawable.ic_baseline_data_usage_24
+                "night", "moonlight", "bluelightfilter" -> R.drawable.ic_baseline_nights_stay_24
+                "smarthome", "home" -> R.drawable.ic_baseline_home_24
+                "privacy" -> R.drawable.ic_baseline_security_24
+                "camera" -> R.drawable.ic_baseline_camera_24
+                "lowpower", "battery_saver", "batterymode", "powerplanning" -> R.drawable.ic_baseline_battery_plus_24
+                "exitkft" -> R.drawable.ic_baseline_child_care_24
+                "recordscreentile", "screenrecorder" -> R.drawable.ic_baseline_videocam_24
+                "screencapture" -> R.drawable.ic_baseline_camera_24
+                "internet" -> R.drawable.ic_network
+                "controls" -> R.drawable.ic_baseline_home_24
+                "mictoggle" -> R.drawable.microphone
+                "cameratoggle" -> R.drawable.ic_baseline_camera_24
+                "screenrecord" -> R.drawable.record
+                "wallet" -> R.drawable.credit_card
+                else -> R.drawable.ic_baseline_android_24
+            },
+            theme
+        )
+    }
+
+    private fun Context.getDefaultDrawable(): Drawable? {
+        return ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_android_24, theme)
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/data/SavedOption.kt
@@ -0,0 +1,26 @@
+package com.zacharee1.systemuituner.data
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+
+data class SavedOption(
+    val type: SettingsType,
+    val key: String,
+    var value: String?
+) {
+    companion object {
+        val gson: Gson = GsonBuilder().create()
+
+        fun fromString(input: String): SavedOption {
+            return try {
+                gson.fromJson(
+                    input,
+                    object : TypeToken<SavedOption>() {}.type
+                )
+            } catch (e: Exception) {
+                //Prevent crashes when options have been saved the old way
+                val split = input.split(":")
+
+                SavedOption(
+                    Setting
