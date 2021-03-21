9d0fc281ecda79d3526c7719db9e76d5b165dd27
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-03-21 14:53:03 +0000

    slight modification

--- app/src/main/java/com/zacharee1/systemuituner/dialogs/CustomBlacklistItemDialogFragment.kt
+++ app/src/main/java/com/zacharee1/systemuituner/dialogs/CustomBlacklistItemDialogFragment.kt
@@ -1,3 +1,27 @@
 package com.zacharee1.systemuituner.dialogs
 
 import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import androidx.preference.PreferenceDialogFragmentCompat
+import com.google.android.material.textfield.TextInputLayout
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.data.CustomBlacklistItemInfo
+import com.zacharee1.systemuituner.util.prefManager
+
+class CustomBlacklistItemDialogFragment : PreferenceDialogFragmentCompat() {
+    companion object {
+        fun newInstance(key: String): CustomBlacklistItemDialogFragment {
+            val instance = CustomBlacklistItemDialogFragment()
+            val args = Bundle()
+
+            args.putString(BaseOptionDialog.ARG_KEY, key)
+
+            instance.arguments = args
+
+            return instance
+        }
+    }
+
+    override fun onCreate
