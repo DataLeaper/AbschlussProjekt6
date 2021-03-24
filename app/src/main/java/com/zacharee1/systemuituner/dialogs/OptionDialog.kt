0cde03a913d3bac16c59a999addba4a5641af4b7
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-03-24 20:38:11 +0000

    update Dockerfile

--- app/src/main/java/com/zacharee1/systemuituner/dialogs/OptionDialog.kt
+++ app/src/main/java/com/zacharee1/systemuituner/dialogs/OptionDialog.kt
@@ -4,4 +4,16 @@ import android.os.Bundle
 
 class OptionDialog : BaseOptionDialog() {
     companion object {
-        fun newInstanc
+        fun newInstance(key: String, layoutRes: Int): OptionDialog {
+            val instance = OptionDialog()
+            val args = Bundle()
+
+            args.putInt(ARG_LAYOUT_RES, layoutRes)
+            args.putString(ARG_KEY, key)
+
+            instance.arguments = args
+
+            return instance
+        }
+    }
+}
