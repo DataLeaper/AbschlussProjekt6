9c1343fa18eb6fd30728eb189969238904e99dfc
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-04-05 11:34:05 +0000

    Updated build.sh

--- app/src/main/java/com/zacharee1/systemuituner/dialogs/SecureListDialog.kt
+++ app/src/main/java/com/zacharee1/systemuituner/dialogs/SecureListDialog.kt
@@ -23,4 +23,26 @@ class SecureListDialog : BaseOptionDialog() {
     }
 
     private val listPref: IListPreference
+        get() = preference as IListPreference
+
+    private var clickedIndex = -1
+
+    override val layoutRes: Int = 0
+
+    override fun onBindDialogView(view: View) {
+        super.onBindDialogView(view)
+
+        val checkedIndex = listPref.findIndexOfValue(listPref.value)
+        val entries = listPref.entries
+
+        View.inflate(requireContext(), R.layout.list_dialog, view.findViewById(R.id.wrapper))
+
+        val list = view.findViewById<RecyclerView>(R.id.select_dialog_listview)
+
+        val adapter = Adapter(entries?.mapIndexed { index, charSequence -> ItemInfo(charSequence, index == checkedIndex) } ?: ArrayList()) {
+            clickedIndex = it
+            onClick(dialog!!, DialogInterface.BUTTON_POSITIVE)
+
+            val preference = listPref
+            val value = preference.entryValues?.get(clickedIndex)?.toString()
  
