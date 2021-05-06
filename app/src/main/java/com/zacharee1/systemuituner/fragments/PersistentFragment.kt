809596bf3afbea2072cf8d3f5e0550a92a49e4a5
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-05-06 06:26:05 +0000

    link fixed

--- app/src/main/java/com/zacharee1/systemuituner/fragments/PersistentFragment.kt
+++ app/src/main/java/com/zacharee1/systemuituner/fragments/PersistentFragment.kt
@@ -22,4 +22,24 @@ import com.zacharee1.systemuituner.data.SearchIndex
 import com.zacharee1.systemuituner.data.SettingsType
 import com.zacharee1.systemuituner.databinding.CustomPersistentOptionWidgetBinding
 import com.zacharee1.systemuituner.dialogs.CustomPersistentOptionDialogFragment
-import com.zacharee1.systemuituner.dialogs.Rou
+import com.zacharee1.systemuituner.dialogs.RoundedBottomSheetDialog
+import com.zacharee1.systemuituner.interfaces.*
+import com.zacharee1.systemuituner.prefs.InlineActivityPreference
+import com.zacharee1.systemuituner.util.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.*
+
+class PersistentFragment : BasePrefFragment(), SearchView.OnQueryTextListener, SharedPreferences.OnSharedPreferenceChangeListener {
+    override val widgetLayout: Int = Int.MIN_VALUE
+
+    private var currentQuery: String? = null
+
+    private val preferences = ArrayList<PersistentPreference>()
+    private val isLoaded = async {
+        SearchIndex.toInflate.forEach {
+            inflate(it.first)
+        }
+
+//        preferences.addAll(requireContext().prefManager.customPersistentOptions.map {
+//            PersistentPreference.fromCustomPersistentOption(requireConte
