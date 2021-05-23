abf5474bfc42492df789b57f91a92b85bfcc4de8
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-05-23 10:10:58 +0000

    Updated from Publii

---
+++ app/src/main/java/com/zacharee1/systemuituner/interfaces/IColorPreference.kt
@@ -0,0 +1,12 @@
+package com.zacharee1.systemuituner.interfaces
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.drawable.StateListDrawable
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.databinding.CustomPreferenceBinding
+import com.zacharee1.systemuituner.util.setColorFilterCompat
+
+interface IColorPreference 
