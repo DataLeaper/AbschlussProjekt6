1cde0e0d97247b3abd278193b8ef6f19beb6fdcf
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-05-24 18:27:20 +0000

    menus

--- app/src/main/java/com/zacharee1/systemuituner/interfaces/IColorPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/interfaces/IColorPreference.kt
@@ -9,4 +9,30 @@ import com.zacharee1.systemuituner.R
 import com.zacharee1.systemuituner.databinding.CustomPreferenceBinding
 import com.zacharee1.systemuituner.util.setColorFilterCompat
 
-interface IColorPreference 
+interface IColorPreference {
+    var iconColor: Int
+
+    fun bindVH(holder: RecyclerView.ViewHolder)
+}
+
+class ColorPreference(context: Context, attrs: AttributeSet?) :
+    IColorPreference {
+    override var iconColor: Int = Int.MIN_VALUE
+
+    init {
+        if (attrs != null) {
+            val array = context.theme.obtainStyledAttributes(attrs, R.styleable.ColorPreference, 0, 0)
+
+            iconColor = array.getColor(R.styleable.ColorPreference_icon_color, iconColor)
+        }
+    }
+
+    override fun bindVH(holder: RecyclerView.ViewHolder) {
+        val binding = CustomPreferenceBinding.bind(holder.itemView)
+
+        binding.iconFrame.apply {
+            (background as StateListDrawable).apply {
+                val drawable = getStateDrawable(1)
+
+                if (iconColor != Int.MIN_VALUE) {
+                    drawable?.setTint(
