42857c4c28cb1436e5f7718ef7d36591c65503ba
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-05-28 16:52:47 +0000

    removed redundant comment

--- app/src/main/java/com/zacharee1/systemuituner/prefs/ManageQSPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/ManageQSPreference.kt
@@ -1,1 +1,22 @@
-package com.zacharee1.systemuituner.p
+package com.zacharee1.systemuituner.prefs
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.AttributeSet
+import androidx.preference.Preference
+import androidx.preference.SwitchPreference
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.interfaces.DangerousPreference
+import com.zacharee1.systemuituner.interfaces.IDangerousPreference
+import com.zacharee1.systemuituner.util.isComponentEnabled
+
+open class ManageQSPreference(context: Context, attrs: AttributeSet?) : SwitchPreference(context, attrs),
+    Preference.OnPreferenceChangeListener,
+    IDangerousPreference by DangerousPreference(context, attrs) {
+
+    private var manageComponent: ComponentName? = null
+
+    init {
+        isPersistent = false
+        layoutResource = R.layout.custom_pre
