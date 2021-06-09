30d41cd65017ae8ecda46d3a5719ea8696295b87
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-09 07:53:28 +0000

    1.4.15

--- app/src/main/java/com/zacharee1/systemuituner/prefs/demo/DemoSwitchPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/demo/DemoSwitchPreference.kt
@@ -2,4 +2,27 @@ package com.zacharee1.systemuituner.prefs.demo
 
 import android.content.Context
 import android.util.AttributeSet
-imp
+import androidx.preference.PreferenceManager
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.prefs.demo.base.BaseDemoPreference
+
+open class DemoSwitchPreference(context: Context, attrs: AttributeSet) : BaseDemoPreference(context, attrs) {
+    companion object {
+        const val DEFAULT_ENABLED = "1"
+        const val DEFAULT_DISABLED = "0"
+    }
+
+    var enabled =
+        DEFAULT_ENABLED
+    var disabled =
+        DEFAULT_DISABLED
+
+    init {
+        val array = context.theme.obtainStyledAttributes(attrs, R.styleable.SecureSwitchPreference, 0, 0)
+
+        enabled = array.getString(R.styleable.SecureSwitchPreference_enabled_value) ?: DEFAULT_ENABLED
+        disabled = array.getString(R.styleable.SecureSwitchPreference_disabled_value) ?: DEFAULT_DISABLED
+
+        array.recycle()
+
+        layoutResource = R.layout.cu
