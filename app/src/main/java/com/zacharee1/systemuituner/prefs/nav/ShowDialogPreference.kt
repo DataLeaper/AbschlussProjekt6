9f6c7d29a9c5a42879ed2ba691193e6778a228a6
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-12 08:02:38 +0000

    updating docs

--- app/src/main/java/com/zacharee1/systemuituner/prefs/nav/ShowDialogPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/nav/ShowDialogPreference.kt
@@ -1,3 +1,27 @@
 package com.zacharee1.systemuituner.prefs.nav
 
-import android.content.Con
+import android.content.Context
+import android.util.AttributeSet
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.dialogs.RoundedBottomSheetDialog
+
+class ShowDialogPreference(context: Context, attrs: AttributeSet?) : Preference(context, attrs) {
+    private val dialogClass: Class<RoundedBottomSheetDialog>?
+
+    init {
+        dialogClass = if (attrs != null) {
+            val array = context.theme.obtainStyledAttributes(attrs, R.styleable.ShowDialogPreference, 0, 0)
+
+            array.getString(R.styleable.ShowDialogPreference_dialog_class)?.let {
+                @Suppress("UNCHECKED_CAST")
+                context.classLoader.loadClass(it) as Class<RoundedBottomSheetDialog>
+            }
+        } else {
+            null
+        }
+    }
+
+    override fun onClick() {
+        supe
