a3b1deeda692a09d832565b1c2024cfaa58ae696
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-14 13:20:39 +0000

    Copyright headers

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/SecureListPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/SecureListPreference.kt
@@ -9,4 +9,24 @@ import androidx.core.content.edit
 import androidx.core.content.res.TypedArrayUtils
 import com.zacharee1.systemuituner.R
 import com.zacharee1.systemuituner.prefs.secure.base.BaseSecurePreference
-import com.zacharee1.syste
+import com.zacharee1.systemuituner.interfaces.IListPreference
+import com.zacharee1.systemuituner.util.getSetting
+import com.zacharee1.systemuituner.util.verifiers.BaseListPreferenceVerifier
+
+@SuppressLint("RestrictedApi")
+class SecureListPreference(context: Context, attrs: AttributeSet) : BaseSecurePreference(context, attrs),
+    IListPreference {
+    private var verifier: BaseListPreferenceVerifier? = null
+
+    private var setValue: Boolean = false
+    override var value: String? = null
+        set(value) {
+            // Always persist/notify the first time.
+            val changed = !TextUtils.equals(field, value)
+            if (changed || !setValue) {
+                field = value
+                setValue = true
+                try {
+                    persistString(value)
+                } catch (e: ClassCastException) {
+                    
