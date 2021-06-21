3653a22d5a0583469964407d78a816029146044a
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-21 12:22:26 +0000

    Add missing export

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/base/BaseSecurePreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/base/BaseSecurePreference.kt
@@ -3,3 +3,23 @@ package com.zacharee1.systemuituner.prefs.secure.base
 import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.PreferenceManager
+import com.zacharee1.systemuituner.interfaces.*
+import com.zacharee1.systemuituner.prefs.base.BaseDialogPreference
+import com.zacharee1.systemuituner.util.*
+
+open class BaseSecurePreference(context: Context, attrs: AttributeSet) : BaseDialogPreference(context, attrs), ISecurePreference by SecurePreference(
+    context,
+    attrs
+) {
+    override var writeKey: String = ""
+        get() = field.ifBlank { key }
+
+    override fun onAttachedToHierarchy(preferenceManager: PreferenceManager) {
+        super.onAttachedToHierarchy(preferenceManager)
+        initSecure(this)
+    }
+
+    override suspend fun onValueChanged(newValue: Any?, key: String): Boolean {
+        return context.writeSetting(type, writeKey, newValue, dangerous, true)
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AirplaneModeRadiosPreference.kt
@@ -0,0 +1,3 @@
+package com.zacharee1.systemuituner.prefs.secure.specific
+
+import android.content.C
