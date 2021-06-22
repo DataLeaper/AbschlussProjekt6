15a72ecf23c406a0e24f47875013a3776c4f388e
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-22 14:11:53 +0000

    Add travis settings

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AirplaneModeRadiosPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AirplaneModeRadiosPreference.kt
@@ -21,4 +21,21 @@ class AirplaneModeRadiosPreference(context: Context, attrs: AttributeSet) : Base
     )
 
     init {
-        key = "ai
+        key = "airplane_mode_radios"
+
+        setTitle(R.string.special_sub_airplane_mode)
+        setSummary(R.string.special_sub_airplane_mode_desc)
+
+        dialogTitle = title
+        dialogMessage = summary
+        setIcon(R.drawable.ic_baseline_airplanemode_active_24)
+        iconColor = ContextCompat.getColor(context, R.color.pref_color_4)
+    }
+
+    override suspend fun onValueChanged(newValue: Any?, key: String): Boolean {
+        val data = newValue as AirplaneModeRadiosData?
+
+        return context.writeSetting(SettingsType.GLOBAL, Settings.Global.AIRPLANE_MODE_RADIOS, data?.blacklisted, saveOption = true) &&
+                context.writeSetting(SettingsType.GLOBAL, Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS, data?.toggleable, saveOption = true)
+    }
+}
