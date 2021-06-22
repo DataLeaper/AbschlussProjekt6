75dff8b95781f75056b2cab6a99332c8cdd4b4fe
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-22 05:38:01 +0000

    Add more comments

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AirplaneModeRadiosPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AirplaneModeRadiosPreference.kt
@@ -1,3 +1,24 @@
 package com.zacharee1.systemuituner.prefs.secure.specific
 
-import android.content.C
+import android.content.Context
+import android.provider.Settings
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.data.AirplaneModeRadiosData
+import com.zacharee1.systemuituner.interfaces.ISpecificPreference
+import com.zacharee1.systemuituner.prefs.base.BaseDialogPreference
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.writeSetting
+
+class AirplaneModeRadiosPreference(context: Context, attrs: AttributeSet) : BaseDialogPreference(context, attrs),
+    ISpecificPreference {
+    override val keys = hashMapOf(
+        SettingsType.GLOBAL to arrayOf(
+            Settings.Global.AIRPLANE_MODE_RADIOS,
+            Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS
+        )
+    )
+
+    init {
+        key = "ai
