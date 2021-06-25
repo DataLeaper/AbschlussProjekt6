2a978ff89cbc4e54cdea179c3f14f7fd271da40b
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-25 10:48:19 +0000

    Removes console.log

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AnimationScalesPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/AnimationScalesPreference.kt
@@ -1,3 +1,20 @@
 package com.zacharee1.systemuituner.prefs.secure.specific
 
-import 
+import android.content.Context
+import android.provider.Settings
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.data.AnimationScalesData
+import com.zacharee1.systemuituner.interfaces.ISpecificPreference
+import com.zacharee1.systemuituner.prefs.base.BaseDialogPreference
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.SettingsInfo
+import com.zacharee1.systemuituner.util.writeSettingsBulk
+
+class AnimationScalesPreference(context: Context, attrs: AttributeSet) : BaseDialogPreference(context, attrs),
+    ISpecificPreference {
+    override val keys = hashMapOf(
+        SettingsType.GLOBAL to arrayOf(
+            Settings.Global.ANIMATOR_DURATION_SCALE,
+            Settings.Global.TRANSI
