fa637732f18a5a36ae543b328c0c4ade852a953b
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-06-29 21:12:01 +0000

    merge Weblate changes using merge_weblate_changes.py.

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/ImmersiveModePreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/ImmersiveModePreference.kt
@@ -4,4 +4,26 @@ import android.content.Context
 import android.provider.Settings
 import android.util.AttributeSet
 import androidx.core.content.ContextCompat
-import com.zacha
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.interfaces.ISpecificPreference
+import com.zacharee1.systemuituner.prefs.base.BaseDialogPreference
+import com.zacharee1.systemuituner.data.SettingsType
+
+class ImmersiveModePreference(context: Context, attrs: AttributeSet) : BaseDialogPreference(context, attrs),
+    ISpecificPreference {
+    override val keys = hashMapOf(
+        SettingsType.GLOBAL to arrayOf(Settings.Global.POLICY_CONTROL)
+    )
+
+    init {
+        key = "immersive_mode_pref"
+
+        setTitle(R.string.feature_immersive_mode)
+        setSummary(R.string.feature_immersive_mode_desc)
+
+        dialogTitle = title
+        dialogMessage = summary
+        setIcon(R.drawable.ic_baseline_fullscreen_24)
+        iconColor = ContextCompat.getColor(context, R.color.pref_color_4)
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/KeepDeviceOnPluggedPreference.kt
@@ -0,0 +1,7 @@
+
+package com.zacharee1.systemuituner.prefs.secure.specific
+
+import android.content.Context
+import android.provider.Settings
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
