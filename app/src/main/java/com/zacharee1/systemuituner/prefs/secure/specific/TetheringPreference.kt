a67e6d3615005869240b8e4393b92c2e8245e17d
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-03 13:29:55 +0000

    Update project to recommended settings

--- app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/TetheringPreference.kt
+++ app/src/main/java/com/zacharee1/systemuituner/prefs/secure/specific/TetheringPreference.kt
@@ -6,4 +6,25 @@ import android.util.AttributeSet
 import androidx.core.content.ContextCompat
 import com.zacharee1.systemuituner.R
 import com.zacharee1.systemuituner.interfaces.ISpecificPreference
-import com.zacharee1.sy
+import com.zacharee1.systemuituner.prefs.base.BaseDialogPreference
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.getSetting
+import com.zacharee1.systemuituner.util.writeSetting
+
+class TetheringPreference(context: Context, attrs: AttributeSet) : BaseDialogPreference(context, attrs),
+    ISpecificPreference {
+    override val keys = hashMapOf(
+        SettingsType.GLOBAL to arrayOf(
+            Settings.Global.TETHER_DUN_REQUIRED,
+            Settings.Global.TETHER_SUPPORTED
+        )
+    )
+
+    val bothFixed: Boolean
+        get() = context.getSetting(SettingsType.GLOBAL, Settings.Global.TETHER_DUN_REQUIRED) == "0"
+                && context.getSetting(SettingsType.GLOBAL, Settings.Global.TETHER_SUPPORTED) == "true"
+
+    init {
+        key = "tethering_fix"
+
+        setTitle(R
