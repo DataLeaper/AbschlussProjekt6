e3f345abf13c84d0934d79065921dc3184e74dd6
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-06 19:21:18 +0000

    enable tests

--- app/src/main/java/com/zacharee1/systemuituner/services/tiles/DemoTile.kt
+++ app/src/main/java/com/zacharee1/systemuituner/services/tiles/DemoTile.kt
@@ -5,4 +5,34 @@ import android.os.Build
 import android.service.quicksettings.Tile
 import com.zacharee1.systemuituner.util.DemoController
 import com.zacharee1.systemuituner.util.safeUpdateTile
-impo
+import kotlinx.coroutines.launch
+
+@TargetApi(Build.VERSION_CODES.N)
+class DemoTile : CoroutineTileService(), (Boolean) -> Unit {
+    private val demoHandler by lazy { DemoController.getInstance(this) }
+
+    override fun onStartListening() {
+        demoHandler.updateListeners.add(this)
+
+        updateState()
+    }
+
+    override fun onStopListening() {
+        demoHandler.updateListeners.remove(this)
+    }
+
+    override fun invoke(enabled: Boolean) {
+        updateState()
+    }
+
+    private fun updateState() {
+        qsTile?.state = if (demoHandler.isCurrentlyEnabled) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile?.safeUpdateTile()
+    }
+
+    override fun onClick() {
+        launch {
+            if (demoHandler.isCurrentlyEnabled) {
+                demoHandler.exitDemo()
+            } else {
+              
