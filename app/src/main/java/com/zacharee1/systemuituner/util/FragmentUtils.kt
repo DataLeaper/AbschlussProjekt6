4eaa79db53c1f724c1cfb0f8bbc0a86af67062c8
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-11 22:27:15 +0000

    Update testing

--- app/src/main/java/com/zacharee1/systemuituner/util/FragmentUtils.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/FragmentUtils.kt
@@ -8,4 +8,34 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlin.math.floor
 
 fun Fragment.chooseLayoutManagerWithoutSetting(
-    view: 
+    view: View?,
+    grid: RecyclerView.LayoutManager,
+    linear: RecyclerView.LayoutManager,
+    extraFlags: Boolean = true,
+): RecyclerView.LayoutManager {
+    val dpWidth = requireContext().asDp(view?.width ?: 0)
+
+    return if (extraFlags && dpWidth >= 800) {
+        grid
+    } else {
+        linear
+    }
+}
+
+fun Fragment.updateLayoutManager(
+    view: View?,
+    recycler: RecyclerView?,
+    grid: RecyclerView.LayoutManager,
+    linear: RecyclerView.LayoutManager,
+    extraFlags: Boolean = true
+) {
+    recycler?.layoutManager = chooseLayoutManager(view, grid, linear, extraFlags)
+}
+
+fun Fragment.chooseLayoutManager(
+    view: View?,
+    grid: RecyclerView.LayoutManager,
+    linear: RecyclerView.LayoutManager,
+    extraFlags: Boolean = true,
+    spanCount: (Float) -> Int = {
+        floor(it 
