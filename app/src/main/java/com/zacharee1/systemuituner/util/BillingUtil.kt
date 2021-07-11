c545b10d2ed2f2842de4ff25e0ffca9ad5d443f4
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-11 08:03:48 +0000

    ....I..... [ZBXNEXT-973] redesign of existing templates

--- app/src/main/java/com/zacharee1/systemuituner/util/BillingUtil.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/BillingUtil.kt
@@ -10,4 +10,23 @@ import com.zacharee1.systemuituner.databinding.LayoutDonateBinding
 import com.zacharee1.systemuituner.dialogs.DonateDialog
 import kotlinx.coroutines.*
 
-class BillingUtil(private val dialo
+class BillingUtil(private val dialog: DonateDialog) : CoroutineScope by MainScope() {
+    private val client: BillingClient
+
+    private val activity: Activity = dialog.context.extractActivity()
+
+    init {
+        client = BillingClient.newBuilder(dialog.context).setListener { response, purchases ->
+            if (response.responseCode == OK && purchases != null) {
+                for (purchase in purchases) {
+                    consumeAsync(purchase.purchaseToken)
+                }
+            }
+        }.enablePendingPurchases().build()
+
+        val dialogBinding = LayoutDonateBinding.bind(dialog.view)
+
+        client.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(result: BillingResult) {
+                dialog.view.post {
+                    dialogBinding.paypalTitle
