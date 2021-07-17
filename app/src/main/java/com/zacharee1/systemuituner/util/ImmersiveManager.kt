f1bf6bffd0867b2651fae48626a80b3ce4e8eb77
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-17 06:29:07 +0000

    fix: remove console.log

--- app/src/main/java/com/zacharee1/systemuituner/util/ImmersiveManager.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/ImmersiveManager.kt
@@ -5,4 +5,26 @@ import android.content.ContextWrapper
 import android.provider.Settings
 import com.zacharee1.systemuituner.data.SettingsType
 
-class ImmersiveManager(con
+class ImmersiveManager(context: Context) : ContextWrapper(context) {
+    enum class ImmersiveMode(val type: String) {
+        NONE("immersive.none"),
+        STATUS("immersive.status"),
+        NAV("immersive.navigation"),
+        FULL("immersive.full")
+    }
+
+    data class ImmersiveInfo(
+        var allFull: Boolean = false,
+        var allStatus: Boolean = false,
+        var allNav: Boolean = false,
+        val fullApps: ArrayList<String> = ArrayList(),
+        val fullBl: ArrayList<String> = ArrayList(),
+        val statusApps: ArrayList<String> = ArrayList(),
+        val statusBl: ArrayList<String> = ArrayList(),
+        val navApps: ArrayList<String> = ArrayList(),
+        val navBl: ArrayList<String> = ArrayList()
+    ) {
+        fun clear() {
+            allFull = false
+            allStatus = false
+            allNav 
