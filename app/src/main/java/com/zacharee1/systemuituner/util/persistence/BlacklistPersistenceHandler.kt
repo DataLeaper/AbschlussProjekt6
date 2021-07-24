3283556147f9a11757fbe03a4ea19b75a4927023
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-24 07:34:38 +0000

    Alias

--- app/src/main/java/com/zacharee1/systemuituner/util/persistence/BlacklistPersistenceHandler.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/persistence/BlacklistPersistenceHandler.kt
@@ -1,3 +1,24 @@
 package com.zacharee1.systemuituner.util.persistence
 
-i
+import android.content.Context
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.prefManager
+import com.zacharee1.systemuituner.util.writeSetting
+
+class BlacklistPersistenceHandler(context: Context) : BasePersistenceHandler<HashSet<String>>(context) {
+    override val settingsKey: String = "icon_blacklist"
+    override val settingsType: SettingsType = SettingsType.SECURE
+
+    override fun getPreferenceValue(): HashSet<String> {
+        return context.prefManager.blacklistedItems
+    }
+
+    override fun getPreferenceValueAsString(): String {
+        return getPreferenceValue().joinToString(",")
+    }
+
+    override fun savePreferenceValue(value: String?) {
+        if (value == null) {
+            context.prefManager.blacklistedItems = HashSet()
+        } else {
+            context.prefManager.blackl
