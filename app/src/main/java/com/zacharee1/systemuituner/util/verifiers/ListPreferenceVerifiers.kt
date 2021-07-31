09fc18e075d90055ba499509055c84a66e1236b7
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-31 06:11:19 +0000

    wrap long line

--- app/src/main/java/com/zacharee1/systemuituner/util/verifiers/ListPreferenceVerifiers.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/verifiers/ListPreferenceVerifiers.kt
@@ -15,4 +15,16 @@ class StorageVerifier(context: Context) : BaseListPreferenceVerifier(context) {
         entries: Array<CharSequence?>?,
         values: Array<CharSequence?>?
     ): Pair<Array<CharSequence?>?, Array<CharSequence?>?> {
-        if (entries == null |
+        if (entries == null || values == null) return Pair<Array<CharSequence?>?, Array<CharSequence?>?>(null, null)
+
+        val entryList = ArrayList(entries.toList())
+        val valueList = ArrayList(values.toList())
+
+        if (!context.hasSdCard) {
+            entryList.removeAt(entryList.lastIndex)
+            valueList.removeAt(valueList.lastIndex)
+        }
+
+        return entryList.toTypedArray() to valueList.toTypedArray()
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/util/verifiers/PreferenceEnabledVerifiers.kt
@@ -0,0 +1,10 @@
+package com.zacharee1.systemuituner.util.verifiers
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.os.Build
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.util.hasSdCard
+import com.zacharee1.systemuituner.util.isTouchW
