0073cfcdf842a08f4e0a7b1eed9edfde1363fcab
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-07-31 07:09:46 +0000

    Added 5 tweets

--- app/src/main/java/com/zacharee1/systemuituner/util/verifiers/PreferenceEnabledVerifiers.kt
+++ app/src/main/java/com/zacharee1/systemuituner/util/verifiers/PreferenceEnabledVerifiers.kt
@@ -7,4 +7,25 @@ import android.content.res.Resources
 import android.os.Build
 import com.zacharee1.systemuituner.R
 import com.zacharee1.systemuituner.util.hasSdCard
-import com.zacharee1.systemuituner.util.isTouchW
+import com.zacharee1.systemuituner.util.isTouchWiz
+
+abstract class BasePreferenceEnabledVerifier(internal val context: Context) {
+    abstract val shouldBeEnabled: Boolean
+    abstract val message: CharSequence?
+}
+
+class EnableStorage(context: Context) : BasePreferenceEnabledVerifier(context) {
+    override val shouldBeEnabled: Boolean
+        get() = context.hasSdCard
+    override val message: CharSequence?
+        get() = context.resources.getString(R.string.compatibility_message_storage)
+}
+
+class EnableLockscreenShortcuts(context: Context) : BasePreferenceEnabledVerifier(context) {
+    override val shouldBeEnabled: Boolean
+        @SuppressLint("DiscouragedApi")
+        get() {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) return true
+            if (context.isTouchWiz) return true
+
+            val resNames = arrayOf("config
