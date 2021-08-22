f6d89dd6842a548a13eefee949bda59f577fc09a
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-08-22 13:57:18 +0000

    Fix conflicts.

--- app/src/main/java/com/zacharee1/systemuituner/views/CameraGestures.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/CameraGestures.kt
@@ -7,3 +7,76 @@ import android.util.AttributeSet
 import android.widget.ScrollView
 import com.zacharee1.systemuituner.data.CameraGesturesData
 import com.zacharee1.systemuituner.databinding.CameraGesturesBinding
+import com.zacharee1.systemuituner.interfaces.IOptionDialogCallback
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.getSetting
+import com.zacharee1.systemuituner.util.launch
+
+class CameraGestures(context: Context, attrs: AttributeSet) : ScrollView(context, attrs), IOptionDialogCallback {
+    override var callback: (suspend (data: Any?) -> Boolean)? = null
+    private val cameraData = CameraGesturesData()
+
+    private val binding by lazy { CameraGesturesBinding.bind(this) }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        cameraData.cameraGestureDisabled = context.getSetting(SettingsType.SECURE, Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1)?.toIntOrNull() ?: 1
+        cameraData.doubleTapPowerDisabled = context.getSetting(SettingsType.SECURE, Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1)?.toIntOrNull() ?: 1
+        cameraData.doubleTwistToFlipEnabled = context.getSetting(SettingsType.SECURE, Settings.Secure.CAMERA_DOUBLE_TWIST_TO_FLIP_ENABLED, 0)?.toIntOrNull() ?: 1
+
+        binding.cameraGesture.apply {
+            isChecked = cameraData.cameraGestureDisabled == 0
+            setOnCheckedChangeListener { _, isChecked ->
+                val newValue = if (isChecked) 0 else 1
+
+                if (newValue != cameraData.cameraGestureDisabled) {
+                    cameraData.cameraGestureDisabled = if (isChecked) 0 else 1
+
+                    launch {
+                        if (callback?.invoke(cameraData) == false) {
+                            cameraData.cameraGestureDisabled = if (isChecked) 1 else 0
+                            this@apply.isChecked = !isChecked
+                        }
+                    }
+                }
+            }
+        }
+
+        binding.cameraPowerButton.apply {
+            isChecked = cameraData.doubleTapPowerDisabled == 0
+            setOnCheckedChangeListener { _, isChecked ->
+                val newValue = if (isChecked) 0 else 1
+
+                if (newValue != cameraData.doubleTapPowerDisabled) {
+                    cameraData.doubleTapPowerDisabled = if (isChecked) 0 else 1
+
+                    launch {
+                        if (callback?.invoke(cameraData) == false) {
+                            cameraData.doubleTapPowerDisabled = if (isChecked) 1 else 0
+                            this@apply.isChecked = !isChecked
+                        }
+                    }
+                }
+            }
+        }
+
+        binding.cameraTwist.apply {
+            isChecked = cameraData.doubleTwistToFlipEnabled == 1
+            setOnCheckedChangeListener { _, isChecked ->
+                val newValue = if (isChecked) 0 else 1
+
+                if (newValue != cameraData.doubleTwistToFlipEnabled) {
+                    cameraData.doubleTwistToFlipEnabled = if (isChecked) 0 else 1
+
+                    launch {
+                        if (callback?.invoke(cameraData) == false) {
+                            cameraData.doubleTwistToFlipEnabled = if (isChecked) 1 else 0
+                            this@apply.isChecked = !isChecked
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/zacharee1/systemuituner/views/DisableableImageView.kt
@@ -0,0 +1,22 @@
+package com.zacharee1.systemuituner.views
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+
+open class DisableableImageView : AppCompatImageView {
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var alphaAnim: ValueAnimator? = null
+
+    override fun setEnabled(enabled: Boolean) {
+        if (this.isEnabled != enabled) {
+            alphaAnim?.cancel()
+
+            alphaAnim = ValueAnimator.ofInt(this.imageAlpha, if (enabled) 0xFF else 0x3F)
+                .apply {
+                    addUpdateListener {
+   
