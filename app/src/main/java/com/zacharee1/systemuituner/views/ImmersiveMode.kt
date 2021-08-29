733981f1a831ad4c1f663d2175024af6cba9abdc
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-08-29 20:28:41 +0000

    Fixed conflicts

--- app/src/main/java/com/zacharee1/systemuituner/views/ImmersiveMode.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/ImmersiveMode.kt
@@ -11,4 +11,23 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.zacharee1.systemuituner.IImmersiveSelectionCallback
 import com.zacharee1.systemuituner.R
-import com.zacharee1.systemuituner.activities.Imm
+import com.zacharee1.systemuituner.activities.ImmersiveListSelector
+import com.zacharee1.systemuituner.databinding.ImmersiveModeBinding
+import com.zacharee1.systemuituner.databinding.ImmersiveModeItemBinding
+import com.zacharee1.systemuituner.util.ImmersiveManager
+import com.zacharee1.systemuituner.util.launch
+import com.zacharee1.systemuituner.util.prefManager
+
+class ImmersiveMode(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    private val immersiveManager = ImmersiveManager(context)
+    private val immersiveInfo = immersiveManager.parseAdvancedImmersive().apply {
+        immersiveManager.loadInSavedLists(this)
+    }
+
+    private val binding by lazy { ImmersiveModeBinding.bind(this) }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        val list = binding.modeList
+        list.adapter = ImmersiveAdapter(immersiveInfo, immersi
