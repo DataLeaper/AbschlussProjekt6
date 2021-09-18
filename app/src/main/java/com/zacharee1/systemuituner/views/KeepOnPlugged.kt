a86267a75dd56aa9d46183b1ae2e9e2d0375b092
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-09-18 09:21:04 +0000

    Updated travis

--- app/src/main/java/com/zacharee1/systemuituner/views/KeepOnPlugged.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/KeepOnPlugged.kt
@@ -8,4 +8,25 @@ import android.widget.CompoundButton
 import android.widget.ScrollView
 import com.zacharee1.systemuituner.databinding.KeepDevicePluggedDialogBinding
 import com.zacharee1.systemuituner.interfaces.IOptionDialogCallback
-import com.zacharee1.sy
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.util.getSetting
+import com.zacharee1.systemuituner.util.launch
+
+class KeepOnPlugged(context: Context, attrs: AttributeSet) : ScrollView(context, attrs), IOptionDialogCallback {
+    override var callback: (suspend (data: Any?) -> Boolean)? = null
+
+    private val binding by lazy { KeepDevicePluggedDialogBinding.bind(this) }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        val ac = binding.onAc
+        val usb = binding.onUsb
+        val wireless = binding.onWireless
+        val current = context.getSetting(
+            SettingsType.GLOBAL,
+            Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+            0
+        )?.toIntOrNull() ?: 0
+
+        ac.isChecked = current and BatteryManager.BATTERY_PLU
