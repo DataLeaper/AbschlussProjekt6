c01d27767f2fdccdca6c6feffe67e79061031970
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-09-26 08:33:26 +0000

    Nothing important

--- app/src/main/java/com/zacharee1/systemuituner/views/OneUIClockPositionView.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/OneUIClockPositionView.kt
@@ -11,4 +11,28 @@ import com.zacharee1.systemuituner.util.getSetting
 import com.zacharee1.systemuituner.util.launch
 
 class OneUIClockPositionView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs), IOptionDialogCallback {
-   
+    override var callback: (suspend (data: Any?) -> Boolean)? = null
+
+    companion object {
+        const val POSITION_LEFT = "left_clock_position"
+        const val POSITION_MIDDLE = "middle_clock_position"
+        const val POSITION_RIGHT = "right_clock_position"
+    }
+
+    private val binding by lazy { OneUiClockPositionBinding.bind(this) }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        init()
+    }
+
+    private fun init() {
+        val blacklist = context.getSetting(SettingsType.SECURE, "icon_blacklist") ?: ""
+        val currentPosition = when {
+            blacklist.contains(POSITION_RIGHT) -> R.id.position_right
+            blacklist.contains(POSITION_MIDDLE) -> R.id.position_middle
+            else -> R.id.position_left
+        }
+
+        binding.clockPo
