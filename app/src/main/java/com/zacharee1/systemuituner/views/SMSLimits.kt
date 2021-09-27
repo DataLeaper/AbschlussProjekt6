0ae5c71b2c1d203581a3512abb2d39ed7f0eff9d
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-09-27 21:27:43 +0000

    Update config.txt

--- app/src/main/java/com/zacharee1/systemuituner/views/SMSLimits.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/SMSLimits.kt
@@ -9,3 +9,22 @@ import com.zacharee1.systemuituner.data.SettingsType
 import com.zacharee1.systemuituner.databinding.SmsLimitsBinding
 import com.zacharee1.systemuituner.util.*
 import kotlinx.coroutines.launch
+
+class SMSLimits(context: Context, attrs: AttributeSet) : ScrollView(context, attrs) {
+    companion object {
+        private const val COUNT_DEF = 30
+        private const val INTERVAL_DEF = 1800000
+    }
+
+    private val binding by lazy { SmsLimitsBinding.bind(this) }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        binding.maxCount.editText?.setText(context.getSetting(SettingsType.GLOBAL, Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, COUNT_DEF))
+        binding.interval.editText?.setText(context.getSetting(SettingsType.GLOBAL, Settings.Global.SMS_OUTGOING_CHECK_INTERVAL_MS, INTERVAL_DEF))
+
+        binding.maxCount.setStartIconOnClickListener {
+            launch {
+                context.writeSetting(SettingsType.GLOBAL, Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, COUNT_DEF, saveOption = true)
+       
