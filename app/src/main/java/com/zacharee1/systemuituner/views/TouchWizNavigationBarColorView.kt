0c998b24513c453ce0e59575905649653172cd2d
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-10-02 11:55:12 +0000

    Registration page

--- app/src/main/java/com/zacharee1/systemuituner/views/TouchWizNavigationBarColorView.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/TouchWizNavigationBarColorView.kt
@@ -7,4 +7,18 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 import com.jaredrummler.android.colorpicker.ColorPickerDialog
-import com.jaredrummler.android.colorpicker.ColorPickerDialogLis
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener
+import com.jaredrummler.android.colorpicker.ColorShape
+import com.zacharee1.systemuituner.R
+import com.zacharee1.systemuituner.data.SettingsType
+import com.zacharee1.systemuituner.databinding.TouchwizNavigationBarColorDialogBinding
+import com.zacharee1.systemuituner.interfaces.IOptionDialogCallback
+import com.zacharee1.systemuituner.util.*
+
+class TouchWizNavigationBarColorView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs), ColorPickerDialogListener, IOptionDialogCallback {
+    override var callback: (suspend (data: Any?) -> Boolean)? = null
+
+    private val dialog: ColorPickerDialog = ColorPickerDialog.newBuilder()
+        .setDialogType(ColorPickerDialog.TYPE_PRESETS)
+        .setDialogTitle(R.string.option_touchwiz_navbar_color)
+        .setColorShape(ColorShape.CIRCLE
