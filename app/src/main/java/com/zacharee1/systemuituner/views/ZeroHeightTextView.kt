1f285ebc76a8642a22d75a548011e8668b63eabc
DataLeaper <118620179+DataLeaper@users.noreply.github.com>
2021-10-03 17:50:42 +0000

    Fix make distcheck

--- app/src/main/java/com/zacharee1/systemuituner/views/ZeroHeightTextView.kt
+++ app/src/main/java/com/zacharee1/systemuituner/views/ZeroHeightTextView.kt
@@ -6,4 +6,29 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import com.zacharee1.systemuituner.util.mainHandler
 
-class ZeroHe
+class ZeroHeightTextView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        mainHandler.post {
+            updateHeight()
+        }
+    }
+
+    override fun setText(text: CharSequence?, type: BufferType?) {
+        super.setText(text, type)
+
+        mainHandler.post {
+            updateHeight()
+        }
+    }
+
+    private fun updateHeight() {
+        layoutParams?.apply {
+            height = if (text.isNullOrBlank()) 0
+            else ViewGroup.LayoutParams.WRAP_CONTENT
+
+            layoutParams = this
+        }
+    }
+}
